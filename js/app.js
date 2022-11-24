78ecce5c940563badeb59433e72790fd7873c5b2
azazmuzaffar <sp18-bcs-035@cuiwah.edu.pk>
2022-11-24 14:43:11 +0500

    Rest of the pages and finalizing fonts!

--- js/app.js
+++ js/app.js
@@ -2,52 +2,32 @@
 /* >>>>>>>>>>>>>>>>>>> Start <<<<<<<<<<<<<<<<<<< */
 /* >>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<  */
 
-(function () {
-  var headings = ["Devenez <br/> <span>indépendant.e</span>,", "Poursuivez <br/> <span>votre passion</span>,", "Valorisez <br/> <span>votre talent</span>,"],
-    i = 0;
-  setInterval(function () {
-    $("#headings").fadeOut(function () {
-      $(this)
-        .html(headings[(i = (i + 1) % headings.length)])
-        .fadeIn();
-    });
-  }, 3000);
-})();
-$(document).ready(function () {
-  $("#menu").click(function () {
-    $(this).toggleClass("open");
-    $(".navbar ul").slideToggle(400);
-    $(".navbar").toggleClass("open");
+$(document).ready(function() {
+  $('#select').niceSelect();
+
+  let selected = $(".nice-select .list .option.selected").attr('data-value');
+  selected = parseInt(selected);
+  let btn = $(".form-connexion .btn");
+
+  if (selected > 0)  {
+    btn.removeClass("disabled");
+  } else {
+    btn.addClass("disabled");
+  }
+
+  $(".nice-select .list .option").click(function(){
+    let selected = $(this).attr('data-value');
+    let btn = $(".form-connexion .btn");
+  
+    selected = parseInt(selected);
+    
+    if (selected > 0)  {
+      btn.removeClass("disabled");
+    } else {
+      btn.addClass("disabled");
+    }
   });
 });
 
-var $slickEl = $(".timings");
 
-$slickEl.slick({
-  focusOnSelect: true,
-  centerPadding: "0px",
-  dots: false,
-  arrows: true,
-  prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-  nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
-  infinite: false,
-  slidesToShow: 5,
-  responsive: [
-    {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 2,
-      },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-      },
-    },
-  ],
-});
 
-/* >>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<  */
-/* >>>>>>>>>>>>>>>>>>>>> End <<<<<<<<<<<<<<<<<<<  */
-/* >>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<  */
